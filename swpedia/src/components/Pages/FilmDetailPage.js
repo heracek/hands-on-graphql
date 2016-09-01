26867e3d7cbcf1d9838d6b6b4937d89ab7fb55e7
Tomáš Horáček <tomas.horacek@gmail.com>
2016-09-01 12:10:09 +0200

    Add InfoList and use it in Planet and Film detail

--- swpedia/src/components/Pages/FilmDetailPage.js
+++ swpedia/src/components/Pages/FilmDetailPage.js
@@ -2,29 +2,43 @@ import React, { Component } from 'react';
 import { Col, Grid, PageHeader } from 'react-bootstrap';
 
 import { PlanetsTable } from '../Planets/PlanetsTable.js';
+import { InfoList } from '../InfoList/InfoList.js';
 
 export class FilmDetailPage extends Component {
   render() {
     const { filmId: id } = this.props.params;
     const {
+      ...film,
       title,
       openingCrawl,
       planets,
     } = {
       title: 'Some Film Title',
       openingCrawl: "It is a dark time for the\r\nRebellion. Although the Death\r\nStar has been destroyed,\r\nImperial troops have driven the\r\nRebel forces from their hidden\r\nbase and pursued them across\r\nthe galaxy.\r\n\r\nEvading the dreaded Imperial\r\nStarfleet, a group of freedom\r\nfighters led by Luke Skywalker\r\nhas established a new secret\r\nbase on the remote ice world\r\nof Hoth.\r\n\r\nThe evil lord Darth Vader,\r\nobsessed with finding young\r\nSkywalker, has dispatched\r\nthousands of remote probes into\r\nthe far reaches of space....",
+      director: 'George Lucas',
+      releaseDate: '1977-05-25',
       planets: [
         { id: 1, name: 'Tatooine' },
         { id: 3, name: 'Yavin IV' },
       ],
     };
 
+    const infoItems = [
+      { key: 'director', header: 'Director'},
+      { key: 'releaseDate', header: 'Release Date'},
+    ];
+
     return (
       <div>
         <PageHeader>{title} <small>ID: {id}</small></PageHeader>
         <Grid>
           <Col xs={6}>
-            <h2>Opening openingCrawl</h2>
+            <h2>Film Info</h2>
+            <InfoList
+              data={film}
+              items={infoItems}
+            />
+            <h2>Opening Crawl</h2>
             <pre>{openingCrawl}</pre>
           </Col>
           <Col xs={6}>
--- swpedia/src/components/Pages/PlanetDetailPage.js
+++ swpedia/src/components/Pages/PlanetDetailPage.js
@@ -1,39 +1,45 @@
 import React, { Component } from 'react';
-import { Col, Grid, ListGroup, ListGroupItem, PageHeader } from 'react-bootstrap';
+import { Col, Grid, PageHeader } from 'react-bootstrap';
 
 import { FilmsTable } from '../Films/FilmsTable.js';
+import { InfoList } from '../InfoList/InfoList.js';
 
 export class PlanetDetailPage extends Component {
   render() {
     const { planetId: id } = this.props.params;
     const {
+      ...planet,
       name,
-      population,
-      climate,
-      diameter,
       films,
     } = {
       name: 'Some Planet',
       population: 8000,
       climate: 'temperate, tropical',
       diameter: 10200,
+      surface_water: 10,
       films: [
         { id: 1, title: 'A New Hope' },
         { id: 3, title: 'Return of the Jedi' },
       ],
     };
 
+    const infoItems = [
+      { key: 'population', header: 'Population'},
+      { key: 'diameter', header: 'Diameter', suffix: ' km'},
+      { key: 'climate', header: 'Climate'},
+      { key: 'surface_water', header: 'Surface Water', suffix: '%'},
+    ];
+
     return (
       <div>
         <PageHeader>{name} <small>ID: {id}</small></PageHeader>
         <Grid>
           <Col xs={6}>
-            <h2>Info</h2>
-            <ListGroup>
-              <ListGroupItem header="Population">{population}</ListGroupItem>
-              <ListGroupItem header="Climate">{climate}</ListGroupItem>
-              <ListGroupItem header="Diameter">{diameter}</ListGroupItem>
-            </ListGroup>
+            <h2>Planet Info</h2>
+            <InfoList
+              data={planet}
+              items={infoItems}
+            />
           </Col>
           <Col xs={6}>
             <h2>Films</h2>
