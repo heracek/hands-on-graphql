4c9d6640645a413e6a79a9580bbacba366e7aa7d
Tomáš Horáček <tomas.horacek@gmail.com>
2016-09-12 13:40:48 +0200

    Fix CORS for GraphQL server

--- graphql-server/server.js
+++ graphql-server/server.js
@@ -5,6 +5,20 @@ import schema from './src/schema';
 const GRAPHQL_PORT = process.env.GRAPHQL_PORT || 8844;
 
 const graphQLServer = express();
+
+(function enableCORS() {
+  graphQLServer.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+  graphQLServer.options('*', (req, res) => {
+    res.sendStatus(200);
+  });
+})();
+
+
 graphQLServer.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
 
 graphQLServer.listen(GRAPHQL_PORT, () => {
